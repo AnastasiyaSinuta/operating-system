77ca0c2c1f279a7b8970f0aefd8fd8c160e09cae
AnastasiyaSinuta <anastasiya.sinyuta2003@gmail.com>
2023-06-17 20:20:04 +0300

    commit

--- lab2/lab2_linux.c
+++ lab2/lab2_linux.c
@@ -1,18 +1,19 @@
-#include <unistd.h>
-#include <sys/mman.h>
-#include <stdio.h>
-
-int main() {
-    while (1) {
-        long size = 1024*1024*1024;
-        void* p = mmap(0, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-        if (p != NULL) {
-            char fullst[16];
-            fgets(fullst, 16, stdin);
-            for (int i = 0; i < size; i += sysconf(_SC_PAGESIZE))
-                ((char*)p)[i] = 0;
-            fgets(fullst, 16, stdin);
-        }
-    }
-    return 0;
+#include <unistd.h> 
+#include <sys/mman.h> 
+#include <stdio.h> 
+ 
+int main() { 
+    while (1) { 
+        long size = sysconf(_SC_PAGESIZE); 
+        void* p = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); 
+        if (p == MAP_FAILED) { 
+            perror("Memory allocation failed"); 
+            break; 
+        } 
+ 
+        for (int i = 0; i < size; i++) 
+            ((char*)p)[i] = 0; 
+    } 
+ 
+    return 0; 
 }
