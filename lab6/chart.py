66e8e1795c9c920c372c54e36255e74779d2f847
AnastasiyaSinuta <anastasiya.sinyuta2003@gmail.com>
2023-06-20 06:27:01 +0300

    lab6

--- lab6/chart.py
+++ lab6/chart.py
@@ -2,17 +2,24 @@ import matplotlib.pyplot as plt
 
 filename = 'allocation_times.txt'
 sizes = []
-times = []
+mallocs = []
+callocs = []
+reallocs = []
 with open(filename, 'r') as file:
-    next(file)  # Пропускаем заголовок файла
+    label0, label1, label2, label3 = file.readline().strip().split(';')
     for line in file:
-        size, time = line.strip().split(';')        
+        size, malloc, calloc, realloc = line.strip().split(';')
         sizes.append(int(size))
-        times.append(float(time))
-plt.plot(sizes, times)
+        mallocs.append(int(malloc))
+        callocs.append(int(calloc))
+        reallocs.append(int(realloc))
+plt.plot(sizes, mallocs, label=label1)
+plt.plot(sizes, callocs, label=label2)
+plt.plot(sizes, reallocs, label=label3)
 plt.xlabel('Размер памяти (байты)')
 plt.ylabel('Время выделения (секунды)')
 plt.title('Зависимость времени выделения от размера памяти')
 plt.grid(True)
+plt.legend()
 plt.savefig('chart.png')
 plt.show()
--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,29 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/time.h> 
 
 #define FILENAME "allocation_times.txt"
-
-int main() {
-    double time_spent = 0.0;
+#define NUM_TESTS 10 
+ 
+long long get_current_time() { 
+    struct timeval tv; 
+    gettimeofday(&tv, NULL); 
+    return tv.tv_sec * 1000000 + tv.tv_usec; 
+} 
+ 
+long long test_malloc(size_t size) { 
+    void* ptr; 
+    long long start_time, end_time, total_time = 0; 
+ 
+    for (int i = 0; i < NUM_TESTS; i++) { 
+        start_time = get_current_time(); 
+        ptr = malloc(size); 
+        free(ptr); 
+        end_time = get_current_time(); 
+        total_time += end_time - start_time;
+    } 
+ 
+    return total_time / NUM_TESTS;
+} 
+ 
+long long test_calloc(size_t size) { 
+    void* ptr; 
+    long long start_time, end_time, total_time = 0; 
+ 
+    for (int i = 0; i < NUM_TESTS; i++) { 
+        start_time = get_current_time(); 
+        ptr = calloc(1, size); 
+        free(ptr); 
+        end_time = get_current_time(); 
+        total_time += end_time - start_time;
+    } 
+ 
+    return total_time / NUM_TESTS;
+} 
+ 
+long long test_realloc(size_t size) { 
+    void* ptr; 
+    long long start_time, end_time, total_time = 0; 
+ 
+    for (int i = 0; i < NUM_TESTS; i++) { 
+        ptr = malloc(size / 2);
+        start_time = get_current_time(); 
+        ptr = realloc(ptr, size);
+        free(ptr);
+        end_time = get_current_time(); 
+        total_time += end_time - start_time; 
+    } 
+ 
+    return total_time / NUM_TESTS;
+} 
+ 
+int main() { 
     FILE* file = fopen(FILENAME, "w");
-    
     if (file == NULL) {
         printf("Failed to open file for writing.\n");
         return 1;
     }
-
-    fprintf(file, "size;time\n");
+    fprintf(file, "size;malloc;calloc;realloc\n");
     
-    for (int i = 0; i < 10000; i++) {
-        clock_t begin = clock();
-        void* memory = malloc(i);
-        free(memory);
-        clock_t end = clock();
-        time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-        fprintf(file, "%d;%f\n", i, time_spent);
-    }
+    size_t min_size = 1;  // Minimum size of memory to allocate 
+    size_t max_size = 100000000;  // Maximum size of memory to allocate 
+    size_t step_size = 100000;  // Size increment 
+ 
+    for (size_t size = min_size; size <= max_size; size += step_size) { 
+        long long m = test_malloc(size); 
+        long long c = test_calloc(size); 
+        long long r = test_realloc(size); 
+        fprintf(file, "%zu;%lld;%lld;%lld\n", size, m, c, r);
+    } 
     fclose(file);
     printf("Results saved to file %s\n", FILENAME);
-    return 0;
+    return 0; 
 }
